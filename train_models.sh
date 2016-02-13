f1912811b26b72eb43f62e0bdb42145371eff69b
Alex Irpan <xelanapri@gmail.com>
2016-02-12 16:24:27 -0800

    Running some LSTM training because why not

--- train_models.sh
+++ train_models.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-declare -a arr=("full" "rnn")
+declare -a arr=("full" "lstm")
 
 for epslen in `seq 2 9`
 do
     for typ in "${arr[@]}"
     do
-        declare -a name="models_${epslen}_${typ}_fixed"
+        declare -a name="models_${epslen}_${typ}"
         mkdir $name
         th train_rubiks.lua --epsLen $epslen --saveDir $name --type $typ
     done
