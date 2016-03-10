3995f8cef24f1d980febf5e0b656cac47c13f4e9
Alex Irpan <xelanapri@gmail.com>
2016-03-09 18:02:28 -0800

    Doubling training and validation size to see if model needs more data

--- train_models.sh
+++ train_models.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 declare -a arr=("lstm")
 
-: '
-for epslen in `seq 2 9`
+for epslen in `seq 5 9`
 do
-    for typ in "${arr[@]}"
-    do
-        declare -a name="models_${epslen}_${typ}"
-        mkdir $name
-        th train_rubiks.lua --epslen $epslen --savedir $name --type $typ
-    done
+    declare -a name="models_more_data/${epslen}_lstm"
+    mkdir $name
+    th train_rubiks.lua --epslen $epslen --savedir $name --type $typ --ntrain 100000 --ntest 20000 --gpu 1
 done
-'
 
+: '
 declare -a name="models_26_lstm"
 mkdir $name
 th train_rubiks.lua --epsLen 26 --saveDir $name --type lstm
+'
 
