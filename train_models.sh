9c2724b5f08480e8c22af7ba0d2d6676d1674010
Alex Irpan <xelanapri@gmail.com>
2016-03-09 18:44:22 -0800

    Forgot to specify type. Now running

--- train_models.sh
+++ train_models.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-declare -a arr=("lstm")
-
 for epslen in `seq 5 9`
 do
     declare -a name="models_more_data/${epslen}_lstm"
     mkdir $name
-    th train_rubiks.lua --epslen $epslen --savedir $name --type $typ --ntrain 100000 --ntest 20000 --gpu 1
+    th train_rubiks.lua --epslen $epslen --savedir $name --type lstm --ntrain 100000 --ntest 20000 --gpu 1
 done
 
 : '
