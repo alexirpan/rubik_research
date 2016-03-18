f54c37d2af43337de7dd033f8593340df8ead766
Alex Irpan <xelanapri@gmail.com>
2016-03-18 14:35:40 -0700

    Modified training script

--- train_models.sh
+++ train_models.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-for epslen in `seq 5 9`
+for epslen in {9,26}
 do
-    declare -a name="models_more_data/${epslen}_lstm"
+    declare -a name="models_streaming/${epslen}_lstm"
     mkdir $name
-    th train_rubiks.lua --epslen $epslen --savedir $name --type lstm --ntrain 100000 --ntest 20000 --gpu 1
+    th train_rubiks.lua --epslen $epslen --savedir $name --type lstm --ntrain 50000 --ntest 10000 --gpu 1
 done
-
-: '
-declare -a name="models_26_lstm"
-mkdir $name
-th train_rubiks.lua --epsLen 26 --saveDir $name --type lstm
-'
 
