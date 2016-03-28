973897d51c4277893c2da8c4a389603467ee8b95
Alex Irpan <xelanapri@gmail.com>
2016-03-28 15:54:19 -0700

    Updating .gitignore to track output files, saving new models

--- train_models.sh
+++ train_models.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-for epslen in {9,26}
-do
-    declare -a name="models_streaming/${epslen}_lstm"
-    mkdir $name
-    th train_rubiks.lua --epslen $epslen --savedir $name --type lstm --ntrain 50000 --ntest 10000 --gpu 1
-done
+declare -a epslen=26
+declare -a name="models_streaming/${epslen}_lstm"
+mkdir $name
+th train_rubiks.lua --epslen $epslen --savedir $name --type lstm --ntrain 50000 --ntest 10000 --gpu 1
 
