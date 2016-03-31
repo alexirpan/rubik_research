f349fd2a3110c8bba7578bc7140b9b48e9471040
Alex Irpan <xelanapri@gmail.com>
2016-03-31 16:11:04 -0700

    Manually hacking CSV for 9 LSTM. Training for longer time period

--- train_models.sh
+++ train_models.sh
@@ -2,5 +2,10 @@
 declare -a epslen=26
 declare -a name="models_streaming/${epslen}_lstm"
 mkdir $name
-th train_rubiks.lua --epslen $epslen --savedir $name --model models_streaming/26_lstm/rubiks_epoch40 --epochs 40 --ntrain 50000 --ntest 10000 --gpu 1
+th train_rubiks.lua --epslen $epslen --savedir $name --model "${name}/rubiks_epoch80" --epochs 40 --ntrain 50000 --ntest 10000 --gpu 1
+
+declare -a epslen=9
+declare -a name="models_streaming/${epslen}_lstm"
+mkdir $name
+th train_rubiks.lua --epslen $epslen --savedir $name --model "${name}/rubiks_epoch40" --epochs 80 --ntrain 50000 --ntest 10000 --gpu 1
 
