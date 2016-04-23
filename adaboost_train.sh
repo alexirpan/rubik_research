3b12581a34624e6cae6efae9611c0626141b959e
Alex Irpan <xelanapri@gmail.com>
2016-04-23 13:42:01 -0700

    Training a bit for the 26 LSTM too

--- adaboost_train.sh
+++ adaboost_train.sh
@@ -3,3 +3,9 @@ declare -a epslen=9
 declare -a name="models_ada/${epslen}_lstm"
 mkdir $name
 th train_rubiks.lua --type lstm --epslen $epslen --savedir $name --epochs 40 --ntrain 50000 --adaboost 1 --batchsize 1 --keepprob 0.9 --gpu 1 | tee "${name}/stdout"
+
+# Only running 10 epochs in case of space issues
+declare -a epslen=26
+declare -a name="models_ada/${epslen}_lstm"
+mkdir $name
+th train_rubiks.lua --type lstm --epslen $epslen --savedir $name --epochs 10 --ntrain 50000 --adaboost 1 --batchsize 1 --keepprob 0.9 --gpu 1 | tee "${name}/stdout"
