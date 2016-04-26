47bf551e048648d1a06ad014182a8939739b9b16
Alex Irpan <xelanapri@gmail.com>
2016-04-26 02:27:40 -0700

    Running another training script overnight

--- adaboost_train.sh
+++ adaboost_train.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
+# Only 20 epochs because of time issues
 declare -a epslen=9
-declare -a name="models_ada/${epslen}_lstm"
+declare -a name="models_ada_mislabel/${epslen}_lstm"
 mkdir $name
-th train_rubiks.lua --type lstm --epslen $epslen --savedir $name --epochs 40 --ntrain 50000 --adaboost 1 --batchsize 1 --keepprob 0.9 --gpu 1 | tee "${name}/stdout"
+th train_rubiks.lua --type lstm --epslen $epslen --savedir $name --epochs 20 --ntrain 50000 --adaboost 1 --batchsize 1 --keepprob 0.8 --gpu 1 | tee "${name}/stdout"
 
-# Only running 10 epochs in case of space issues
-declare -a epslen=26
-declare -a name="models_ada/${epslen}_lstm"
-mkdir $name
-th train_rubiks.lua --type lstm --epslen $epslen --savedir $name --epochs 10 --ntrain 50000 --adaboost 1 --batchsize 1 --keepprob 0.9 --gpu 1 | tee "${name}/stdout"
